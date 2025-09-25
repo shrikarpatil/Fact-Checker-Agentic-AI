12b2e6097e705664133f8306d9524faa3efbf855
Shrikar Patil <72334546+shrikarpatil@users.noreply.github.com>
2025-09-25 11:18:55 +0530

    Update getLLM.py

--- getLLM.py
+++ getLLM.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 def getLLM():
     api_key = os.getenv("OLLAMA_API_KEY")
-    api_endpoint = os.getenv("OLLAMA_API_ENDPOINT", "https://ollama.com")
-    llm = OllamaLLM(model="llama3", api_key=api_key, api_endpoint=api_endpoint,mode="cloud")   
+    api_endpoint = os.getenv("OLLAMA_API_ENDPOINT")
+    llm = OllamaLLM(model="llama3", host=api_endpoint)   
     return llm
 
