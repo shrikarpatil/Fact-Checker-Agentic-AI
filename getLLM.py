0b9b54167d51e1d7909c616f7f3ac00178e571d4
Shrikar <shrikarpatil1012@gmail.com>
2025-09-25 19:39:52 +0530

    Open ai api added

--- getLLM.py
+++ getLLM.py
@@ -1,12 +1,15 @@
 import os
 from dotenv import load_dotenv
-from langchain_ollama import OllamaLLM
+from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 def getLLM():
-    api_key = os.getenv("OLLAMA_API_KEY")
-    api_endpoint = os.getenv("OLLAMA_API_ENDPOINT", "https://ollama.com")
-    llm = OllamaLLM(model="llama3", api_key=api_key, api_endpoint=api_endpoint,mode="cloud")   
-    return llm
+     api_key = os.getenv("API_KEY")
+     llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        api_key=api_key,
+        temperature=0.7
+    )
+     return llm
 
